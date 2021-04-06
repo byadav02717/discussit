0a1088ffb60533d50991b6647c866b96c130fe71
arkbuilder117 <73303907+arkbuilder117@users.noreply.github.com>
2021-04-05 21:18:15 -0500

    Updated about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import '../css/About.css';
 
 export default function About() {
     return (
-        <div>
-            <h1>about us page</h1>
+        <div className="container">
+            <div className="row">
+                <h1>About</h1> 
+            </div>
+            <div className="row">
+                <div className="statement">
+                    <h4>DisscussIt is a new, independent classroom tool designed to streamline communication between teachers and students.</h4>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col s6">
+                    <h5>Picture will go here</h5>
+                </div>
+                <div className="col s6">
+                    <h5>Free Q&A Format</h5>
+                    <p>Users have to ability to post questions that can be answered by the entire class. Giving all students the ability to contribute in a meaningful way, and best of all everything is completely free.</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col s6">
+                    <h5>Customized Groups</h5>
+                    <p>Groups are created by teachers and have the ability to customize them as they see fit with custom tags for questions and complete control over who has access to the group.</p>
+                </div>
+                <div className="col s6">
+                    <h5>Picture will go here</h5>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col s6">
+                    <h5>Picture will go here</h5>
+                </div>
+                <div className="col s6">
+                    <h5>Streamlined UI</h5>
+                    <p>User interface is streamlined allow easy access to all of our groups and to find specific questions quickly.</p>
+                </div>
+            </div>
         </div>
     )
 }
