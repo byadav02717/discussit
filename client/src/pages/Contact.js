10c6704aae823247f443ed695ecaaf2c8f1c5ec0
arkbuilder117 <73303907+arkbuilder117@users.noreply.github.com>
2021-04-05 21:19:49 -0500

    Updated contact page
    Still needs additional work

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function Contact() {
     return (
-        <div>
-            <h1> Contact page</h1>
+        <div className="container">
+            <div className="row">
+                <h1>Contact Us</h1> 
+            <div className="row">  
+                <h5>For any questiosn, send us an email at info.disscussit@gmail.com 
+                    
+                </h5>
+            </div>
+            </div>
         </div>
     )
 }
